8ccb278a81e5234d3f98aaa2946bb2db8faafe89
1-mec <matteojonas3@gmail.com>
2026-09-13 00:45:32 +0200

    first commit

---
+++ JEE/src/main/java/fr/univ/jee/ex2/ServletWelcom.java
@@ -0,0 +1,53 @@
+package fr.univ.jee.ex2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class ServletWelcom
+ */
+@WebServlet("/ServletWelcom")
+public class ServletWelcom extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletWelcom() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		response.setContentType("text/html");
+		HttpSession session = request.getSession(false);
+
+		response.getWriter().append("<html><body><h1><center>");
+		if (session == null) {
+			response.getWriter().append("You don't have any sessions lil bro");
+		} else {
+			response.getWriter().append( "Welcome "+ session.getAttribute("log") + " !" );
+		}
+		response.getWriter().append("</center></h1></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
